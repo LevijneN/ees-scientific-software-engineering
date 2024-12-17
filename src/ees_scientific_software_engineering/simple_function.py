96b399f9d4a9d529c3f46ecf583c5a95c7b9abe4
LevijneN <c.l.nieuwenhuyzen@tue.nl>
2024-12-17 12:35:46 +0100

    check nan
    Signed-off-by: LevijneN <c.l.nieuwenhuyzen@tue.nl>

--- src/ees_scientific_software_engineering/simple_function.py
+++ src/ees_scientific_software_engineering/simple_function.py
@@ -33,9 +33,10 @@ def rms(input_array: np.ndarray) -> float:
         raise TypeError("Dimension of array should be 1")
     if input_array.dtype != np.float64:
         raise TypeError("Array must only contain float64")
-
     if len(input_array) == 0:
         raise TypeError("The size of input_array should be at least 1!")
+    if np.any(np.isnan(input_array)):
+        raise TypeError("Array must not have NaN!")
 
     squared = np.square(input_array)  # Step 1: Square each element
     mean_of_squares = np.mean(squared)  # Step 2: Compute the mean of the squared values
