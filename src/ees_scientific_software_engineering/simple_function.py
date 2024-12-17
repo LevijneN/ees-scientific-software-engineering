7a4a7ba71549fddfcdc32de3400d72fa71435c68
LevijneN <c.l.nieuwenhuyzen@tue.nl>
2024-12-17 10:22:57 +0100

    solve test error
    Signed-off-by: LevijneN <c.l.nieuwenhuyzen@tue.nl>

--- src/ees_scientific_software_engineering/simple_function.py
+++ src/ees_scientific_software_engineering/simple_function.py
@@ -28,5 +28,5 @@ def multiply(a: int, b: int) -> int:
 def rms(input_array: np.ndarray) -> float:
     squared = np.square(input_array)  # Step 1: Square each element
     mean_of_squares = np.mean(squared)  # Step 2: Compute the mean of the squared values
-    rms = np.sqrt(mean_of_squares)  # Step 3: Take the square root
-    return rms
+    rms_result = np.sqrt(mean_of_squares)  # Step 3: Take the square root
+    return rms_result
